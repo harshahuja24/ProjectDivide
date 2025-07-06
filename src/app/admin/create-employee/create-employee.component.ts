3d5b274c11142d3c791833125e27b94d1d524c85
harshahuja24 <harshnahuja@gmail.com>
2025-07-06 12:48:49 +0530

    connection w backend and employee display done

--- src/app/admin/create-employee/create-employee.component.ts
+++ src/app/admin/create-employee/create-employee.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { EmployeeService } from 'src/app/shared/services/employee.service';
 
 @Component({
   selector: 'app-create-employee',
@@ -7,6 +8,17 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent {
+
+  employeesList!:any
+  isAdminFlag!:boolean
+
+  ngOnInit(){
+    this.viewAllEmployees()
+  }
+
+  constructor(private employeeService: EmployeeService){
+
+  }
 
   isEmployeeModalOpen = false;
 
@@ -20,6 +32,48 @@ export class CreateEmployeeComponent {
 
   employeeSubmit(){
     console.log(this.employeeRegistration.value)
+    let backendVal:any = {};
+    backendVal = {
+      "ename": this.employeeRegistration.value.ename,
+      "isAdmin":this.employeeRegistration.value.isAdmin
+    }
+  this.employeeService.createEmployee(backendVal).subscribe({
+    next: (res: any) => {
+    this.viewAllEmployees();
+    },
+    error: (err: any) => {
+    // handle error, e.g., show an error message
+    console.error('Error creating employee:', err);
+    }
+  });
+
+  this.isEmployeeModalOpen = false;
+    
+
   }
   
+
+  viewAllEmployees(){
+    this.employeeService.viewAllEmployees().subscribe((e:any)=>{
+      console.log(e)
+      // console.log(e['isAdmin'])
+      // this.employeesList=e
+      // if(e['isAdmin'] == true) {
+      //   this.isAdminFlag = true
+      // }
+
+      // console.log(e[0].isAdmin);
+
+      this.employeesList = e.map((employee: any) => {
+        return {
+          ename: employee.ename,
+          isAdmin: employee.isAdmin
+        };
+      });
+
+      console.log(this.employeesList);
+  });
+    
+  }
+
 }
---
+++ src/app/shared/services/employee.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  constructor(private http:HttpClient) { }
+
+
+  createEmployee(body:any){
+    return this.http.post("http://localhost:8080/employee/createEmployee",body);
+  }
+
+  viewAllEmployees(){
+    return this.http.get("http://localhost:8080/employee/getAllEmployees")
+  }
+}
