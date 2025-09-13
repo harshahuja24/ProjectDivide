374d6222143aa50eb9794e1d9078875d50a525fa
SimranGurdasani <simrangurdasani05@gmail.com>
2025-09-13 21:06:46 +0530

    user validation done.

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { EmployeeService } from 'src/app/shared/services/employee.service';
 
 @Component({
   selector: 'app-login',
@@ -12,11 +14,41 @@ export class LoginComponent {
     password: new FormControl('',[Validators.required, Validators.minLength(6)]),
   });
 
+  constructor(private employeeService: EmployeeService,private router:Router) {}
+
+  ngOnInit() {
+    this.employeeService.getAllEmployees().subscribe((employees: any[]) => {
+      console.log('Fetched employees on init:', employees);
+      employees.forEach(emp => console.log(emp.password));
+
+    });
+  }
+
   onSubmit() {
     console.log(this.loginForm.value);
+    this.validateUserFromBackend(this.loginForm.value);
   }
+
+  validateUserFromBackend(formData: any) {
+    this.employeeService.getAllEmployees().subscribe((employees: any[]) => {
+      console.log('Fetched employees:', employees);
+      const user = employees.find(emp => emp.email === formData.email && emp.password === formData.password);
+      if (user) {
+        console.log('Login successful:', user);
+        localStorage.setItem('loggedInUserId', user.id.toString());
+        // Redirect to dashboard or another page
+        this.router.navigate(['']);
+      } else {
+        console.log('Invalid email or password');
+      }
+
+  })
+
+}
+
 
   forgotPassword() {
     console.log('Forgot password clicked');
   }
+
 }
